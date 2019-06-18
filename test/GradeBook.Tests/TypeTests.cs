5cf65dbae29658bd526ce94fe17862236b8ceb91
George Obetta <eobetta2@illibnois.edu>
2019-06-18 15:47:23 +0100

    testing commits

--- test/GradeBook.Tests/TypeTests.cs
+++ test/GradeBook.Tests/TypeTests.cs
@@ -7,13 +7,22 @@ namespace GradeBook.Tests
 
     {
         [Fact]
-        public void Test1() {
+        public void GetBookHasDistinctReturns() {
             Book book = GetBook("Book 1");
             Book book2 = GetBook("Book 2");
 
             Assert.Equal("Book 1", book.Name);
             Assert.Equal("Book 2", book2.Name);
 
+        }
+
+        [Fact]
+        public void TwoVarsReferenceSameObject() {
+            Book book = GetBook("Book 1");
+            Book book2 = book;
+
+            Assert.Same(book2, book);
+            Assert.True(Object.ReferenceEquals(book,book2));
         }
 
         Book GetBook(string name) {
